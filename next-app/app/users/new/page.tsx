8fa711ee44644f7e6151b6df2861a1b0b8be0096
istamosh <63596969+istamosh@users.noreply.github.com>
2024-08-03 15:37:37 +0700

    trying Nextjs

---
+++ next-app/app/users/new/page.tsx
@@ -0,0 +1,8 @@
+// page accessed through /users/new (like the dir)
+import React from "react";
+
+const NewUserPage = () => {
+  return <div>NewUserPage</div>;
+};
+
+export default NewUserPage;
---
+++ next-app/app/users/page.tsx
@@ -0,0 +1,10 @@
+// Routing system, convention, not configuration
+// rafce <= shortcut for code snippet autocomplete
+// access in browser then /users
+import React from "react";
+
+const UsersPage = () => {
+  return <div>UsersPage</div>;
+};
+
+export default UsersPage;
