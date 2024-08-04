a10b074c7aa74def735072dd4c410f051c77be57
istamosh <63596969+istamosh@users.noreply.github.com>
2024-08-04 19:40:14 +0700

    try to design the intro and navbar section

--- next-app/app/page.tsx
+++ next-app/app/page.tsx
@@ -5,13 +5,27 @@ import ProductCard from "./users/components/ProductCard";
 export default function Home() {
   return (
     <main>
-      <h1>Hello World!</h1>
+      {/* <h1>Hello World!</h1> */}
 
       {/* accessing anchor tag link will re-request layout.css, webpack.js, main-app.js */}
       {/* this will impact performance */}
       {/* instead, use a Link tag */}
       <Link href="/users">User&apos;s Page</Link>
       <ProductCard />
+      <ProductCard />
+
+      <div id="navigation">
+        <ul>
+          <li>About</li>
+          <li>Work</li>
+          <li>Contact</li>
+        </ul>
+      </div>
+
+      <div id="intro">
+        <h1>Salutations! I'm Alfian Ferdinan</h1>
+        <p>I design a website!</p>
+      </div>
     </main>
   );
 }
