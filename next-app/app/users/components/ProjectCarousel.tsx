3da2162c2ce63c16d6c353d66c93b16f9c5255cc
istamosh <63596969+istamosh@users.noreply.github.com>
2024-08-09 23:18:25 +0700

    carousel completed, but need more optimation

--- next-app/app/users/components/ProjectCarousel.tsx
+++ next-app/app/users/components/ProjectCarousel.tsx
@@ -1,9 +1,13 @@
 "use client";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Link from "next/link";
 
 const ProjectCarousel = () => {
   const [currentIndex, setCurrentIndex] = useState(0);
+  // const [image, setImage] = useState([
+  //   "https://img.daisyui.com/images/stock/photo-1625726411847-8cbb60cc71e6.webp",
+  // ]);
+
   const images = [
     "https://img.daisyui.com/images/stock/photo-1625726411847-8cbb60cc71e6.webp",
     "https://img.daisyui.com/images/stock/photo-1609621838510-5ad474b7d25d.webp",
@@ -11,26 +15,33 @@ const ProjectCarousel = () => {
     "https://img.daisyui.com/images/stock/photo-1665553365602-b2fb8e5d1707.webp",
   ];
   const handlePrev = () => {
-    const newIndex = currentIndex === 0 ? images.length - 1 : currentIndex - 1;
-    setCurrentIndex(newIndex);
+    const index = currentIndex === 0 ? images.length - 1 : currentIndex - 1;
+    setCurrentIndex(index);
+    // setImage((prev) => [...prev, images[index]]);
+    // setTimeout(() => {
+    //   setImage(image.slice(1));
+    // }, 1000);
   };
   const handleNext = () => {
-    const newIndex = currentIndex === images.length - 1 ? 0 : currentIndex + 1;
-    setCurrentIndex(newIndex);
+    const index = currentIndex === images.length - 1 ? 0 : currentIndex + 1;
+    setCurrentIndex(index);
+    // setImage((prev) => [images[index], ...prev]);
+    // setTimeout(() => {
+    //   setImage(image.slice(0, -1));
+    // }, 1000);
   };
+
   return (
     <div className="carousel w-[80vw] h-[50vh] rounded-2xl relative">
       <div id="images" className="relative w-full h-full flex">
-        <img
-          src="https://img.daisyui.com/images/stock/photo-1625726411847-8cbb60cc71e6.webp"
-          className="w-full h-full object-cover transition-transform duration-500 ease-in-out"
-          style={{ transform: `translateX(-${currentIndex * 100}%)` }}
-        />
-        <img
-          src="https://img.daisyui.com/images/stock/photo-1609621838510-5ad474b7d25d.webp"
-          className="w-full h-full object-cover transition-transform duration-500 ease-in-out"
-          style={{ transform: `translateX(-${currentIndex * 100}%)` }}
-        />
+        {images.map((src, index) => (
+          <img
+            key={index}
+            src={src}
+            className="w-full h-full object-cover transition-transform duration-500 ease-in-out"
+            style={{ transform: `translateX(-${currentIndex * 100}%)` }}
+          />
+        ))}
 
         <div
           id="navigation-arrow"
