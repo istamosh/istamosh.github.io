417017d714d67ca89005ce8e5ab1565ddb7df9f9
istamosh <63596969+istamosh@users.noreply.github.com>
2024-08-22 22:47:46 +0700

    revising the title

--- app/users/components/Dashboard.tsx
+++ app/users/components/Dashboard.tsx
@@ -4,14 +4,16 @@ const Dashboard = () => {
   return (
     <>
       <div className="text-right px-4 sm:px-16 space-y-3">
-        <h1 className="text-3xl sm:text-9xl">Hello!</h1>
+        <h1 className="text-3xl sm:text-9xl bg-clip-text text-transparent bg-gradient-to-l from-teal-500 via-cyan-500 to-green-500">
+          Hello!
+        </h1>
         <h1 className="text-md sm:text-3xl">I am Alfian Ferdinan</h1>
         <p className="text-xl sm:text-5xl">
-          I am a{" "}
-          <span className="bg-clip-text text-transparent bg-gradient-to-r from-indigo-500 via-sky-500 to-emerald-500">
-            Front End
-          </span>{" "}
-          Dev.
+          Welcome to my{" "}
+          <span className="bg-clip-text text-transparent bg-gradient-to-r from-orange-500 via-amber-500 to-yellow-500">
+            digital playground
+          </span>
+          .
         </p>
       </div>
     </>
