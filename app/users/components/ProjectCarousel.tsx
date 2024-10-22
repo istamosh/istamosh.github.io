e8bf1363e85f2b2d5604d118b7ce01a4ad533e6f
istamosh <63596969+istamosh@users.noreply.github.com>
2024-10-22 16:11:38 +0700

    added meta descriptions on every page, and displayed as project's description, added istamov site

--- app/users/components/ProjectCarousel.tsx
+++ app/users/components/ProjectCarousel.tsx
@@ -2,39 +2,89 @@
 import React, { useEffect, useState } from "react";
 import Image from "next/image";
 
+interface ProjectData {
+  url: string;
+  description: string | null;
+}
+
+interface UrlData {
+  url: string;
+  description: string;
+}
+
 export const ProjectCarousel = () => {
   const [currentIndex, setCurrentIndex] = useState(0);
-  const [projectUrls, setProjectUrls] = useState<string[]>([]);
+  const [projectData, setProjectData] = useState<ProjectData[]>([]);
   const [imageUrls, setImageUrls] = useState<string[]>([]);
   const [loading, setLoading] = useState<boolean>(false);
 
-  const fetchAllUrls = async () => {
-    setLoading(true);
-    try {
-      const projectResponse = await fetch("/api/projects");
-      if (!projectResponse.ok) {
-        throw new Error(
-          "Failed to fetch project paths:" + projectResponse.status
-        );
-      }
-      const projectData = await projectResponse.json();
-
-      const jsonResponse = await fetch("/urls.json");
-      if (!jsonResponse.ok) {
-        throw new Error("Failed to fetch project paths:" + jsonResponse.status);
-      }
-      const jsonData = await jsonResponse.json();
-
-      const combinedUrls = [...(jsonData.urls || []), ...projectData];
-
-      setProjectUrls(combinedUrls);
-    } catch (error) {
-      console.error("Error fetching project paths:", error);
-    }
-    setLoading(false);
-  };
-
   useEffect(() => {
+    const fetchAllUrls = async () => {
+      setLoading(true);
+      try {
+        const projectResponse = await fetch("/api/projects");
+        if (!projectResponse.ok) {
+          throw new Error(
+            "Failed to fetch project paths:" + projectResponse.status
+          );
+        }
+        const projectData = await projectResponse.json();
+
+        const jsonResponse = await fetch("/urlData.json");
+        if (!jsonResponse.ok) {
+          throw new Error(
+            "Failed to fetch project paths:" + jsonResponse.status
+          );
+        }
+
+        const jsonData: UrlData[] = await jsonResponse.json();
+
+        const combinedUrls = [
+          ...(jsonData.map((el) => el.url) || []),
+          ...projectData,
+        ];
+
+        const fetchMetaDescription = async (url: string) => {
+          if (!url.startsWith("/projects/")) {
+            return null;
+          }
+
+          try {
+            const response = await fetch(url);
+            if (!response.ok) {
+              throw new Error(
+                `Failed to fetch project ${url}: + ${response.status}`
+              );
+            }
+            const text = await response.text();
+            const parser = new DOMParser();
+            const doc = parser.parseFromString(text, "text/html");
+            const metaDesc = doc.querySelector("meta[name='description']");
+            return metaDesc
+              ? metaDesc.getAttribute("content")
+              : "No description available.";
+          } catch (error) {
+            // console.error(`Error fetching meta description for ${url}:`, error);
+            return null;
+          }
+        };
+
+        const metaDescriptions = await Promise.all(
+          combinedUrls.map(fetchMetaDescription)
+        );
+
+        const combinedData = combinedUrls.map((url: string, index) => ({
+          url,
+          description: jsonData[index]?.description || metaDescriptions[index],
+        }));
+
+        setProjectData(combinedData);
+      } catch (error) {
+        console.error("Error fetching project paths:", error);
+      }
+      setLoading(false);
+    };
+
     fetchAllUrls();
   }, []);
 
@@ -42,10 +92,10 @@ export const ProjectCarousel = () => {
     const takeScreenshotAPI = async () => {
       setLoading(true);
 
-      const promises = projectUrls.map(async (url) => {
+      const promises = projectData.map(async (el) => {
         try {
           const filename =
-            url.replace(/[^a-zA-Z0-9]/gi, "_").toLowerCase() + ".png";
+            el.url.replace(/[^a-zA-Z0-9]/gi, "_").toLowerCase() + ".png";
           const imageUrl = `/screenshots/${filename}`;
 
           const response = await fetch(imageUrl, { method: "HEAD" });
@@ -54,7 +104,7 @@ export const ProjectCarousel = () => {
           } else {
             // call the route.tsx to take a screenshot
             const screenshotResponse = await fetch(
-              `/api/screenshot?url=${encodeURIComponent(url)}`
+              `/api/screenshot?url=${encodeURIComponent(el.url)}`
             );
 
             const data = await screenshotResponse.json();
@@ -63,14 +113,14 @@ export const ProjectCarousel = () => {
               return data.image;
             } else {
               console.log(
-                `Error fetching screenshot for ${url}:`,
+                `Error fetching screenshot for ${el.url}:`,
                 data.error || data
               );
               return "";
             }
           }
         } catch (error) {
-          console.log(`Error processing ${url}:`, error);
+          console.log(`Error processing ${el.url}:`, error);
           return "";
         }
       });
@@ -80,7 +130,7 @@ export const ProjectCarousel = () => {
     };
 
     takeScreenshotAPI();
-  }, [projectUrls]);
+  }, [projectData]);
 
   const handlePrev = () => {
     setCurrentIndex((prev) => (prev === 0 ? imageUrls.length - 1 : prev - 1));
@@ -94,24 +144,32 @@ export const ProjectCarousel = () => {
       <h1 className="font-sans text-2xl sm:text-5xl text-center mb-5">
         Projects
       </h1>
-      <div className="carousel w-[80vw] h-[50vh] rounded-2xl relative">
+      <div className="carousel w-[80vw] h-[50vh] rounded-2xl relative outline outline-1 outline-sky-400">
         {imageUrls.map((src, index) => (
           <a
             key={index}
-            href={projectUrls[index]}
+            href={projectData[index].url}
             target="_blank"
             className="carousel-item size-full w-full h-auto object-contain flex transition-transform duration-500 ease-in-out"
             style={{ transform: `translateX(-${currentIndex * 100}%)` }}
           >
-            <Image
-              src={src}
-              className="w-full h-auto object-contain"
-              width={400}
-              height={100}
-              quality={70}
-              alt={`Project ${index + 1}`}
-              loading="lazy"
-            />
+            <div className="flex w-full flex-col justify-between">
+              <div className="card bg-base-300 rounded-box grid h-20 place-items-center">
+                <Image
+                  src={src}
+                  className="w-full h-auto object-contain"
+                  width={400}
+                  height={100}
+                  quality={70}
+                  alt={`Project ${index + 1}`}
+                  loading="lazy"
+                />
+              </div>
+              <div className="divider"></div>
+              <div className="card bg-slate-900 rounded-md grid h-15 place-items-center text-center text-cyan-100">
+                {projectData[index].description}
+              </div>
+            </div>
           </a>
         ))}
 
--- scripts/route.tsx
+++ scripts/route.tsx
@@ -1,43 +1,47 @@
-// only use during development, place inside /api/screenshot.tsx
-
+// will only work in development mode
 import { NextRequest, NextResponse } from "next/server";
-import puppeteer from "puppeteer";
+import puppeteer from "puppeteer-core";
 import path from "path";
 import fs from "fs";
 
-export const GET = async (req: NextRequest) => {
-  const { searchParams } = new URL(req.url);
-  const url = searchParams.get("url");
+const isDevelopment = process.env.NEXT_PUBLIC_ENV === "development";
 
-  if (!url || typeof url !== "string") {
-    return NextResponse.json({ error: "Invalid URL" }, { status: 400 });
-  }
+export const GET = isDevelopment
+  ? async (req: NextRequest) => {
+      const { searchParams } = new URL(req.url);
+      const url = searchParams.get("url");
 
-  const filename = url.replace(/[^a-zA-Z0-9]/gi, "_").toLowerCase() + ".png";
-  const screenshotPath = path.join(
-    process.cwd(),
-    "public/screenshots",
-    filename
-  );
+      if (!url || typeof url !== "string") {
+        return NextResponse.json({ error: "Invalid URL" }, { status: 400 });
+      }
 
-  // Check if the screenshot already exists
-  if (fs.existsSync(screenshotPath)) {
-    return NextResponse.json({ image: `/screenshots/${filename}` });
-  }
+      const filename =
+        url.replace(/[^a-zA-Z0-9]/gi, "_").toLowerCase() + ".png";
+      const screenshotPath = path.join(
+        process.cwd(),
+        "public/screenshots",
+        filename
+      );
 
-  try {
-    const browser = await puppeteer.launch();
-    const page = await browser.newPage();
-    await page.goto(url, { waitUntil: "networkidle2" });
-    await page.screenshot({ path: screenshotPath });
-    await browser.close();
+      // Check if the screenshot already exists
+      if (fs.existsSync(screenshotPath)) {
+        return NextResponse.json({ image: `/screenshots/${filename}` });
+      }
 
-    return NextResponse.json({ image: `/screenshots/${filename}` });
-  } catch (error) {
-    console.error("Error taking screenshot:", error);
-    return NextResponse.json(
-      { error: "Failed to take screenshot" },
-      { status: 500 }
-    );
-  }
-};
+      try {
+        const browser = await puppeteer.launch();
+        const page = await browser.newPage();
+        await page.goto(url, { waitUntil: "networkidle2" });
+        await page.screenshot({ path: screenshotPath });
+        await browser.close();
+
+        return NextResponse.json({ image: `/screenshots/${filename}` });
+      } catch (error) {
+        console.error("Error taking screenshot:", error);
+        return NextResponse.json(
+          { error: "Failed to take screenshot" },
+          { status: 500 }
+        );
+      }
+    }
+  : undefined;
