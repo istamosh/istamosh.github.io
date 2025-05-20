a6b7a4b72a3368f4ffbb90809d64e9d11b8474b1
istamosh <63596969+istamosh@users.noreply.github.com>
2025-05-20 16:28:41 +0700

    add: rewrites api route, not tested yet

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,16 @@ const nextConfig: NextConfig = {
   output: "export",
   distDir: "dist",
   reactStrictMode: true,
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination:
+          // "https://deployment.com/api/:path*",
+        "http://localhost:5000/api/:path*",
+      },
+    ];
+  }
 };
 
 export default nextConfig;
