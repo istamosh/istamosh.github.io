0ee1cac63dbe3daf6612fcedcfcdc624050e94ce
istamosh <63596969+istamosh@users.noreply.github.com>
2025-06-23 02:42:03 +0000

    added dynamic API route using env

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,14 @@
 import type { NextConfig } from "next";
 
+const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL || "http://localhost:5000";
+
 const nextConfig: NextConfig = {
   reactStrictMode: true,
   async rewrites() {
     return [
       {
         source: '/api/:path*',
-        destination: 'http://localhost:5000/api/:path*',
+        destination: `${API_BASE_URL}/api/:path*`,
       },
     ];
   },
